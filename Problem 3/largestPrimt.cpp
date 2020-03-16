3cec9da20dafbe0c4a627bed7e687f583af308c5
Jack Burne <jack_burne@yahoo.co.uk>
2020-03-16 14:18:12 +0000

    Small edit to Partial C++ Solution

--- Problem 3/largestPrimt.cpp
+++ Problem 3/largestPrimt.cpp
@@ -13,15 +13,21 @@ void findFactor(double numToFactor, double currDivisor, double largestDivisor) {
 
     // We also run a loop that performs a modulo of our number to factor
     // and our current divisor. If it's not 0, we increment the current
-    // divisor
+    // divisor (We need to use fmod from math.h because "%" isn't declared
+    // for doubles)
     while (fmod(currDivisor, numToFactor) != 0) {
       currDivisor += 1;
     }
 
+    // if our current divisor is larger than our currently Largest Divisor,
     if (currDivisor >= largestDivisor) {
 
+      // Then we set the current divisor to BE our largest...
       largestDivisor = currDivisor;
+      // and set our new number to factor to be the current factor divided by
+      // the current divisor
       numToFactor = numToFactor / currDivisor;
+      // We then reset our current divisor to be 2
       currDivisor = 2;
     } else {
 
@@ -30,6 +36,7 @@ void findFactor(double numToFactor, double currDivisor, double largestDivisor) {
 
     continue;
   }
+
   cout << "Largest possible prime for " + to_string(numFactor) + " is: " + to_string(largestDivisor);
 }
 
