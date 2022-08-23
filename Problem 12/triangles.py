57b2a3a8a342376874438ae4fa005e50132eb8ef
Jack Burne <jack_burne@yahoo.co.uk>
2022-08-23 20:52:36 +0100

    Problem 12 python solution

---
+++ Problem 12/triangles.py
@@ -0,0 +1,33 @@
+# 1. Find the next triangle number (adding all the natural numbers)
+# 2. find all the factors of this number
+# 3. Is it more than 500?
+import math
+
+
+# Function for finding factors of a given number
+def factors(n):
+    currFactors = 0
+
+    # We look at every number up to our current number, "n"
+    for i in range(1, int(math.ceil(math.sqrt(n)))):
+        # If it is a factor (if it divides without a remainder), we add 2 to our count
+        # As all factors are pairs
+        if (n%i == 0):
+            currFactors += 2
+        else:
+            continue
+    
+    return currFactors
+
+
+def main():
+    y = 1
+    for x in range(2, 1000000):
+        y += x
+
+        if factors(y) >= 500:
+            print(y)
+            break
+
+
+main()
